9743a87add869840cfe180f0d57a6a6990a38105
Tserekh Ivan <tserekh.ivan@gmail.com>
2018-09-13 21:21:56 +0300

    13 Sep 2018

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  zeros = 0;
+  for (i = 5; i < number; i*=5) {
+    zeros += Math.floor(number/i);
+  }
+  return zeros;
 }
